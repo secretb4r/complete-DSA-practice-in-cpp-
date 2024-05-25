5d40a8ba519f00089ddde0a943f3019c696df883
secretb4r <biswajitdasbd38064@gmail.com>
2024-05-25 03:39:30 +0000

    queue implementation completed in cpp

--- src/queue.cpp
+++ src/queue.cpp
@@ -9,65 +9,121 @@ class Queue {
   
   public:
   
-  int * queue, *head;
-  int size, base, top, itemCounter;
+  int * queue, *front, *back;
+  int queueSize, size, itemCounter;
   Queue() { queue = NULL;}
   Queue(int _size) {
-    size = _size;
-    queue = new int[size];
-    head = NULL;
-    base = -1, top = 0;
+    queueSize = _size;
+    queue = new int[queueSize];
+    front = back = &queue[0];
     itemCounter = 0;
+    size = -1;
   }
+  
+  // queueSize -> total size of queue
+  // size -> total length of present item in queue
+  // itemCounter -> 
   
   int isFull();
   int isEmpty();
   
   void Enqueue(int Item);
    int Dequeue();   
-  void showData();
-  void updateData();
+  void showQueue();
+  void updateQueueData(int position, int value);
   void showHead();
   void showTail();
 };
 
-void Queue::showData() {
-  int counter = 0;
-  while(counter < size) {
-    std::cout << queue[counter] << "\t";
-    counter++;
-  } 
-}
 
 int Queue::isEmpty() {
-  if(base != -1) return 0;
+  if(size != -1) return 0;
   else return 1;
 } 
 
 int Queue::isFull() {
-  if(itemCounter == size) return 1;
+  if(size == queueSize -1) return 1;
   else return 0;
   
 }
 
 void Queue::Enqueue(int Item) {
  if(isEmpty() || !isFull()) {
-   queue[iteCounter] = Item;
-   if(isEmpty()) base++;
+   size++;
+   queue[size] = Item;
+   back = &queue[size];
    itemCounter++;
-   head = queue[itemCounter];
- }
+  } else {
+   std::cout << "can't push queue is full " << std::endl;
+  }
 }
 
 int Queue::Dequeue() {
   if(isEmpty()) {
    std::cout << "queue is empty " << std::endl;
-   return base;
+   return size;
   }  
-  
   int temp = queue[0];
-  
+  int index = 0;
+  while(index < size) {
+   queue[index] = queue[index + 1];
+   index++;
+  }
+  size--;
+  itemCounter--;  
+  return temp;
 }
+
+void Queue::showHead() {
+  std::cout << "front: " << *front << std::endl;
+}
+
+void Queue::showTail() {
+ std::cout << "back: " << *back << std::endl;
+}
+
+
+void Queue::showQueue() {
+  int index = 0;
+  while(index < itemCounter) {
+   std::cout << queue[index] << std::endl;
+   index++; 
+  }
+}
+
+
+void Queue::updateQueueData(int position, int value) {
+  if(position > queueSize -1 || position < 0) {
+   std::cout << "position out of range , give valid position" << std::endl; 
+  } else if( position > itemCounter-1) {
+   std::cout << " position should be less than or equal size of the queue" <<std::endl;
+  }
+  
+  queue[position] = value;
+}
+ 
+
 int main() {
+
+Queue first(6);
+
+first.Enqueue(12);
+first.Enqueue(23);
+first.Enqueue(34);
+first.Enqueue(67);
+
+
+first.showQueue();
+first.showHead();
+first.showTail();
+std::cout << "size: " << first.itemCounter << "\n";
+
+
+first.Dequeue();
+
+first.showQueue();
+first.showHead();
+first.showTail();
+std::cout << "size: " << first.itemCounter << "\n";
  return 0;
 }
